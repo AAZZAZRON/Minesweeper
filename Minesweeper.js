2e2dd114a13e62a3ad02e4117af7f47cd03e605e
AAZZAZRON <aaronzhu3.1415@gmail.com>
2021-05-12 08:43:48 -0400

    used hidden HTMLs, fixed bugs

--- Minesweeper.js
+++ Minesweeper.js
@@ -1,205 +1,147 @@
 // ----- INITIALIZATION ----- // 
-const body = document.querySelector("body");
 const difficultyRatings = {"E": [8, 10], "M": [14, 18], "H": [20, 24]};
 const difficultyFlags = {"E": 10, "M": 40, "H": 99};
 const moves = [[-1, 0], [0, -1], [0, 1], [1, 0], [1, 1], [-1, -1], [-1, 1], [1, -1]];
-var board, map, visited, isFlagged;
-var newGame, flags, numFlags, time, revealButton, clicktoMenu;
+var map, visited, isFlagged;
+var numFlags, revealButton, clicktoMenu;
 var queue = [];
 var firstMove = true;
 var timer;
 var counter = 0;
 var squaresLeft;
 var done = false;
-mainMenu();
+var body, mainMenuDiv, levelsDiv, rulesDiv, newGameDiv, boardDiv; 
+window.onload = function() {
+    // initialize the "pages"
+    body = document.getElementById("TheBody");
+    mainMenuDiv = document.getElementById("MainMenu");
+    levelsDiv = document.getElementById("Levels");
+    rulesDiv = document.getElementById("Rules");
+    newGameDiv = document.getElementById("NewGame");
+    boardDiv = document.getElementById("TheBoard");
+    revealButton = document.getElementById("reveal-button")
+    clicktoMenu = document.getElementById("back-to-menu-button");
 
+    flags = document.getElementById("d1");
+    time = document.getElementById("d2");
+
+    // add click directs
+    document.getElementById("play-button").onclick = () => chooseLevel();
+    document.getElementById("rules-button").onclick = () => rules();
+    document.getElementById("easy").onclick = () => createNewGame("E");
+    document.getElementById("medium").onclick = () => createNewGame("M");
+    document.getElementById("hard").onclick = () => createNewGame("H");
+    revealButton.onclick = () => revealAll();
+    clicktoMenu.onclick = () => mainMenu();
+
+    mainMenu();
+}
 
 
 /* ----- MAIN MENU STUFF ----- */
 function mainMenu() {
-    // create main menu divs
-    var mainMenu = document.createElement("div");
-    body.appendChild(mainMenu);
-
-    var playButton = document.createElement("button");
-    playButton.innerText = "Click to Play";
-    playButton.className = "play-button";
-    mainMenu.appendChild(playButton);
-
-    var rulesButton = document.createElement("button");
-    rulesButton.innerText = "Rules";
-    rulesButton.className = "rules-button";
-    mainMenu.appendChild(rulesButton);
-
-    playButton.addEventListener("click", () => {
-        mainMenu.remove();
-        chooseLevel();
-    })
-    rulesButton.addEventListener("click", () => {
-        mainMenu.remove();
-        rules();
-    })
+    //hide and unhide
+    clicktoMenu.hidden = true;
+    mainMenuDiv.hidden = false;
+    levelsDiv.hidden = true;
+    rulesDiv.hidden = true;
+    newGameDiv.hidden = true;
 }
 
 function chooseLevel() {
-    // create level buttons
-    var levels = document.createElement("div");
-    body.appendChild(levels);
-    var easy = document.createElement("button");
-    easy.innerText = "Easy"
-    easy.className = "easy";
-    levels.appendChild(easy);
-
-    var medium = document.createElement("button");
-    medium.innerText = "Medium";
-    medium.className = "medium";
-    levels.appendChild(medium);
-
-    var hard = document.createElement("button");
-    hard.innerText = "Hard";
-    hard.className = "hard";
-    levels.appendChild(hard);
-
-    var back = document.createElement("button");
-    back.innerText = "Back";
-    back.className = "back";
-    levels.appendChild(back);
-
-    easy.addEventListener("click", () => {
-        levels.remove();
-        createNewGame("E");
-    })
-
-    medium.addEventListener("click", () => {
-        levels.remove();
-        createNewGame("M");
-    })
-
-    hard.addEventListener("click", () => {
-        levels.remove();
-        createNewGame("H");
-    })
-
-    back.addEventListener("click", () => {
-        levels.remove();
-        mainMenu();
-    })
+    // hide and unhide
+    mainMenuDiv.hidden = true;
+    levelsDiv.hidden = false;
+    rulesDiv.hidden = true;
+    newGameDiv.hidden = true;
 }
 
 function rules() {
-    var rules = document.createElement("div");
-    body.appendChild(rules);
-
-    var para = document.createElement("div");
-    para.innerText = "The basic idea to playing Minesweeper is that you attempt to find the bombs in a grid.\n\nThe grid is completely filled with unknown squares. Some contain empty space, and some contain bombs. Your goal is to reveal all the empty space without hitting a bomb.\n\nTo help you solve this puzzle, Numbers in squares next to unrevealed squares will indicate how many bombs are adjacent to it (corners included).\n\nFor example, if an empty square has a 2 in it, there are 2 bombs next to it.\n\nTo reveal a space you believe doesn't contain a bomb, click on that square with the LEFT mouse button.\n\nTo mark a space you believe contains a bomb, click using your RIGHT mouse button.\n\nYou win if you reveal all the empty spaces without clicking on a bomb.\n\nGood luck!"
-
-    para.className = "rules";
-    rules.appendChild(para);
-
-    var back = document.createElement("button");
-    back.innerText = "Back";
-    back.className = "back";
-    rules.appendChild(back);
-
-    back.addEventListener("click", () => {
-        rules.remove();
-        mainMenu();
-    })
+    mainMenuDiv.hidden = true;
+    levelsDiv.hidden = true;
+    rulesDiv.hidden = false;
+    newGameDiv.hidden = true;
 }
 
 function gameReset() { // reset all game components
-    board = undefined;
     map = undefined;
     visited = undefined;
     isFlagged = undefined;
-    newGame = undefined;
-    flags = undefined;
     numFlags = undefined;
-    time = undefined;
-    revealButton = undefined;
-    clicktoMenu = undefined;
     queue = [];
     firstMove = true;
     timer = undefined;
     counter = 0;
     squaresLeft = undefined;
     done = false;
+    boardDiv.innerHTML = "";
 }
 
 function createNewGame(chosen) {
+    mainMenuDiv.hidden = true;
+    levelsDiv.hidden = true;
+    rulesDiv.hidden = true;
+    newGameDiv.hidden = false;
     gameReset();
-    board = createBoard(chosen); // set up board and flags and time
-    activateBoard(chosen); // create divs for squares
-    resetArrays(); // reset arrays
+    createBoard(chosen); // set up board and flags and time
 }
 
 // ----- SET UP THE BOARD ----- //
 function createBoard(difficulty) {
     numFlags = difficultyFlags[difficulty]; // number of flags
 
-    newGame = document.createElement("div"); // create overall div
     const a = difficultyRatings[difficulty][0]; // a and b, for x and y axes
     const b = difficultyRatings[difficulty][1];
-    body.appendChild(newGame);
+
 
     squaresLeft = (a * b) - numFlags; // flags and time, create divs
-    flags = document.createElement("div");
-    flags.className = "details d1";
-    flags.innerText = `🚩 ${numFlags}`;
-    newGame.appendChild(flags);
+    document.getElementById("flags").innerText = `🚩 ${numFlags}`;
+    document.getElementById("time").innerText = "🕰️ 0";
 
-    time = document.createElement("div");
-    time.className = "details d2";
-    time.innerText = "🕰️ 0";
-    newGame.appendChild(time);
 
-    var y = screen.height / 2 - 40 * (b / 2);
-    if (difficulty == "H") { // screen adjustments
-        y += 40;
-    } else if (difficulty == "E") {
-        y -= 80;
-    }
-
-    flags.style.top = (y - 50) + "px"; // adjustments
-    time.style.top = (y - 50) + "px";
-
-    var x = 0;
     var before = 0;
-    var boardArray = [];
+
     for (let i = 0; i < a; i += 1) {
-        var row = [];
-        x = screen.width / 2 - 40 * (b / 2);
-        y += 40;
+        var row = document.createElement("tr");
         before += 1;
-        for (let j = 0; j < b; j += 1) { // add pieces to an array, give checkedboard colour pattern
-            var newPiece = document.createElement("div");
-            newPiece.className = "board not-visited";
+        for (let j = 0; j < b; j += 1) {
+            var element = document.createElement("td")
+            element.className = "board not-visited";
             if (before % 2 == 1) {
-                newPiece.classList.add("board-colour2");
+                element.classList.add("board-colour2");
             } else {
-                newPiece.classList.add("board-colour1");
+                element.classList.add("board-colour1");
             }
+            // initialize buttons
+            element.addEventListener("click", () => { // left click
+                if (!isFlagged[i][j] && firstMove) { // activates only if said coord is the first move of the game
+                    map = createMap(difficulty, i, j);
+                    revealButton.hidden = false;
+                    firstMove = false;
+                    timer = setInterval(increment, 1000);
+                    done = false;
+                }
+                if (!done && !isFlagged[i][j]) {
+                    flipTile(i, j);
+                }
+            });
+            element.addEventListener("contextmenu", () => { // right click
+                if (visited[i][j]) {
+                } else if (!done && isFlagged[i][j]) { // if it is already flagged
+                    removeFlag(i, j);
+                } else if (!done && !isFlagged[i][j]) { // if it is not flagged
+                    if (numFlags !== 0) {
+                        addFlag(i, j);
+                    }
+                }
+            });
             before += 1;
-            newPiece.style.top = (y) + "px";
-            newPiece.style.left = (x) + "px";
-            newGame.appendChild(newPiece);
-            row.push(newPiece);
-            x += 40;
+            row.appendChild(element);
         }
-        boardArray.push(row);
+        boardDiv.appendChild(row);
     }
-
-    clicktoMenu = document.createElement("button"); // create buttons
-    clicktoMenu.innerText = "Click to Return to Menu";
-    clicktoMenu.className = "play-button";
-    clicktoMenu.style.top = (y + 130) + "px";
-
-    revealButton = document.createElement("button");
-    revealButton.className = "reveal-button";
-    revealButton.innerText = "Click to Reveal Answer"
-    revealButton.style.top = (y + 130) + "px";
-
-    return boardArray;
+    console.log(boardDiv);
+    resetArrays(a, b);
 }
 
 function createMap(difficulty, one, two) { // creating the map such that the first coordinate user clicks is always a zero
@@ -261,55 +203,15 @@ function createMap(difficulty, one, two) { // creating the map such that the fir
     return mapGrid;
 }
 
-function createRevealButton() { // create reveal button
-    newGame.appendChild(revealButton);
-    revealButton.addEventListener("click", () => {
-        revealButton.remove();
-        revealAll();
-    })
-}
-
-function resetArrays() { // reset visited and isFlagged arrays (change from undefined to set size)
-    visited = new Array(board.length);
-    isFlagged = new Array(board.length);
-    for (let i = 0; i < board.length; i += 1) {
-        visited[i] = new Array(board[0].length);
-        isFlagged[i] = new Array(board[0].length);
-        for (let j = 0; j < board[0].length; j += 1) {
+function resetArrays(x, y) { // reset visited and isFlagged arrays (change from undefined to set size)
+    visited = new Array(x);
+    isFlagged = new Array(x);
+    for (let i = 0; i < x; i += 1) {
+        visited[i] = new Array(y);
+        isFlagged[i] = new Array(y);
+        for (let j = 0; j < y; j += 1) {
             visited[i][j] = false;
             isFlagged[i][j] = false;
-        }
-    }
-}
-
-/* ----- ACTIVATE ALL THE BOARD PIECES ----- */
-function activateBoard(difficulty) {
-    const a = difficultyRatings[difficulty][0];
-    const b = difficultyRatings[difficulty][1];
-    for (let i = 0; i < a; i += 1) {
-        for (let j = 0; j < b; j += 1) {
-            board[i][j].addEventListener("click", () => { // left click
-                if (!isFlagged[i][j] && firstMove) { // activates only if said coord is the first move of the game
-                    map = createMap(difficulty, i, j);
-                    createRevealButton();
-                    firstMove = false;
-                    timer = setInterval(increment, 1000);
-                    done = false;
-                }
-                if (!done && !isFlagged[i][j]) {
-                    flipTile(i, j);
-                }
-            }, {once: true});
-            board[i][j].addEventListener("contextmenu", () => { // right click
-                if (visited[i][j]) {
-                } else if (!done && isFlagged[i][j]) { // if it is already flagged
-                    removeFlag(i, j);
-                } else if (!done && !isFlagged[i][j]) { // if it is not flagged
-                    if (numFlags !== 0) {
-                        addFlag(i, j);
-                    }
-                }
-            });
         }
     }
 }
@@ -317,7 +219,7 @@ function activateBoard(difficulty) {
 /* ----- TIMER ----- */
 function increment() {
     counter += 1;
-    time.innerText = `🕰️ ${counter}`;
+    document.getElementById("time").innerText = `🕰️ ${counter}`;
     if (done) {
         clearInterval(timer);
     }
@@ -341,7 +243,7 @@ function flipTile(i, j) {
                     for (let q = 0; q < 8; q += 1) {
                         x = moves[q][0] + coord[0];
                         y = moves[q][1] + coord[1];
-                        if (0 <= x && x < board.length && 0 <= y && y < board[0].length && !visited[x][y]) {
+                        if (0 <= x && x < map.length && 0 <= y && y < map[0].length && !visited[x][y]) {
                             queue.push([x, y]);
                         }
                     }
@@ -356,48 +258,44 @@ function flipTile(i, j) {
 
 function changeHTML(i, j) { // change the background colour of the board
     visited[i][j] = true;
-    if (board[i][j].classList.contains("board-colour1")) {
-        board[i][j].classList.remove("board-colour1");
+    if (boardDiv.rows[i].cells[j].classList.contains("board-colour1")) {
+        boardDiv.rows[i].cells[j].classList.remove("board-colour1");
         if (map[i][j] == "💣") {
-            board[i][j].classList.add("is-bomb");
+            boardDiv.rows[i].cells[j].classList.add("is-bomb");
         } else {
-            board[i][j].classList.add("visited-colour1");
+            boardDiv.rows[i].cells[j].classList.add("visited-colour1");
         }
-    } else if (board[i][j].classList.contains("board-colour2")){
-        board[i][j].classList.remove("board-colour2");
+    } else if (boardDiv.rows[i].cells[j].classList.contains("board-colour2")){
+        boardDiv.rows[i].cells[j].classList.remove("board-colour2");
         if (map[i][j] == "💣") {
-            board[i][j].classList.add("is-bomb");
+            boardDiv.rows[i].cells[j].classList.add("is-bomb");
         } else {
-            board[i][j].classList.add("visited-colour2");
+            boardDiv.rows[i].cells[j].classList.add("visited-colour2");
         }
     }
     if (map[i][j] !== 0) {
-        board[i][j].innerText = map[i][j];
+        boardDiv.rows[i].cells[j].innerText = map[i][j];
     }
 }
 
 /* ----- REVEAL ALL ----- */
 function revealAll() {
     done = true;
-    for (let i = 0; i < board.length; i += 1) {
-        for (let j = 0; j < board[0].length; j += 1) { // loops through map, revealing everything
+    for (let i = 0; i < map.length; i += 1) {
+        for (let j = 0; j < map[0].length; j += 1) { // loops through map, revealing everything
             if (isFlagged[i][j]) {
                 removeFlag(i, j);
             }
             changeHTML(i, j);
             if (map[i][j] == "💣") {
-                board[i][j].style.fontSize = 30 + "px";
+                boardDiv.rows[i].cells[j].style.fontSize = 30 + "px";
             }
         }
     }
-    flags.innerText = "🚩 0";
+    document.getElementById("flags").innerText = "🚩 0";
 
     let _ = setTimeout(() => { // wait three seconds before allowing user to return to main menu
-        newGame.appendChild(clicktoMenu);
-        clicktoMenu.addEventListener("click", () => {
-            newGame.remove();
-            mainMenu();
-        })
+        clicktoMenu.hidden = false;
     }, 3000);
 }
 
@@ -405,48 +303,46 @@ function revealAll() {
 function removeFlag(i, j) { // remove the flag at [i, j], update isFlagged and numFlags
     isFlagged[i][j] = false;
     numFlags += 1;
-    board[i][j].innerText = "";
-    board[i][j].style.fontSize = 40 + "px"; // revert the size
-    flags.innerText = `🚩 ${numFlags}`;
+    boardDiv.rows[i].cells[j].innerText = "";
+    boardDiv.rows[i].cells[j].style.fontSize = 35 + "px"; // revert the size
+    document.getElementById("flags").innerText = `🚩 ${numFlags}`;
 }
 
 function addFlag(i, j) { // add a flag at [i, j], update isFlagged and numFlags
     isFlagged[i][j] = true;
     numFlags -= 1;
-    board[i][j].innerText = "🚩";
-    board[i][j].style.fontSize = 30 + "px"; // change the size
-    flags.innerText = `🚩 ${numFlags}`;
+    boardDiv.rows[i].cells[j].innerText = "🚩";
+    boardDiv.rows[i].cells[j].style.fontSize = 30 + "px"; // change the size
+    document.getElementById("flags").innerText = `🚩 ${numFlags}`;
 }
 
 /* WIN/LOSE/GO BACK TO MENU */
 function lose() {
+    document.getElementById("status").hidden = false;
+    document.getElementById("reveal").hidden = false;
     done = true;
-    var final = document.createElement("div");
-    final.className = "status";
-    final.innerText = "You Lose.";
-    var reveal = document.createElement("div");
-    reveal.className = "reveal";
-    reveal.innerText = "Click to reveal Answer";
-    newGame.appendChild(final);
-    final.appendChild(reveal);
+    document.getElementById("status").innerText = "You Lose.";
+    document.getElementById("reveal").innerText = "Click to reveal Answer";
 
-    revealButton.remove();
-    final.addEventListener("click", () => {
-        final.remove();
+    revealButton.hidden = true;
+    document.getElementById("status").addEventListener("click", () => {
+        document.getElementById("status").hidden = true;
+        document.getElementById("reveal").hidden = true;
         revealAll();
-    });
+    }, {once: true});
+    document.getElementById("reveal").addEventListener("click", () => {
+        document.getElementById("status").hidden = true;
+        document.getElementById("reveal").hidden = true;
+        revealAll();
+    }, {once: true});
 }
 
 function win() {
+    document.getElementById("status").hidden = false;
+    document.getElementById("reveal").hidden = false;
     done = true;
-    var final = document.createElement("div");
-    final.className = "status";
-    final.innerText = "You Win!";
-    var reveal = document.createElement("div");
-    reveal.className = "reveal";
-    reveal.innerText = `Final Time: ${counter + 1} Seconds`;
-    newGame.appendChild(final);
-    final.appendChild(reveal);
-    revealButton.remove();
+    document.getElementById("status").innerText = "You Win!";
+    document.getElementById("reveal").innerText = `Final Time: ${counter + 1} Seconds`;
+    revealButton.hidden = true;
     revealAll();
 }
